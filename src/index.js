3604419d8c1e395e085573aa671d95807749992a
li.yabin <li.yabin@neusoft.com>
2021-04-25 22:53:47 +0800

    切换新的分支,重写了jshint，明天有时间成eslint吧

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './style.css'
-import TestImage from '../asset/image.jpg'
-import PrintHelloWorld from './print'
+import './style.css';
+import TestImage from '../asset/image.jpg';
+import PrintHelloWorld from './print';
 
 function component() {
   const element = document.createElement('div');
@@ -8,31 +8,31 @@ function component() {
   // 将按钮添加到我们已经存在的 div 中。
   const btn = document.createElement('button');
   btn.innerHTML = 'Button';
-  btn.onclick = PrintHelloWorld
+  btn.onclick = PrintHelloWorld;
   element.appendChild(btn);
 
   // 测试字体1
-  const testFont1 = document.createElement('p')
-  testFont1.innerHTML = 'Demo1 Of Test Font, you can see the change'
-  testFont1.classList.add('font1')
-  element.appendChild(testFont1)
+  const testFont1 = document.createElement('p');
+  testFont1.innerHTML = 'Demo1 Of Test Font, you can see the change';
+  testFont1.classList.add('font1');
+  element.appendChild(testFont1);
 
   // 测试字体2
-  const testFont2 = document.createElement('p')
-  testFont2.innerHTML = 'Demo2 Of Test Font, you can see the change'
-  testFont2.classList.add('font2')
-  element.appendChild(testFont2)
+  const testFont2 = document.createElement('p');
+  testFont2.innerHTML = 'Demo2 Of Test Font, you can see the change';
+  testFont2.classList.add('font2');
+  element.appendChild(testFont2);
 
   // 测试字体3
-  const testFont3 = document.createElement('p')
-  testFont3.innerHTML = 'Demo3 Of Test Font,you can see the change'
-  element.appendChild(testFont3)
+  const testFont3 = document.createElement('p');
+  testFont3.innerHTML = 'Demo3 Of Test Font,you can see the change';
+  element.appendChild(testFont3);
 
   // 将图像添加到我们已经存在的 div 中。
   const myImage = new Image();
   myImage.src = TestImage;
-  myImage.height = 100
-  element.appendChild(myImage)
+  myImage.height = 100;
+  element.appendChild(myImage);
 
   return element;
 }
